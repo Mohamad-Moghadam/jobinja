cee815b73d2d29de6bfcfa3ac9055fac0faf57df
mhmmdmous <muhammadmousaviar7@gmail.com>
2025-03-11 14:33:16 +0330

    M V 1.5

--- jobinja/user/urls.py
+++ jobinja/user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import user_welcome
+from .views import user_welcome, user_cookies
 
 
 urlpatterns = [
-    path('', user_welcome)
+    path('', user_welcome),
+    path('cookies', user_cookies),
 ]
--- jobinja/user/views.py
+++ jobinja/user/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, FileResponse
+import os
+
 
 def user_welcome(request):
     return render(request, 'user/main.html')
+
+def user_cookies(request):
+    file = os.path.join('C:/Users/mhmmd/Desktop/test1/practice12-4/jobinja/user/templates/user/cookies.txt')
+    return FileResponse(open(file, 'rb'), as_attachment=True)

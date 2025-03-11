bb2ec8af593dbcbd3d87a8c65ceb182514020868
mhmmdmous <muhammadmousaviar7@gmail.com>
2025-03-11 15:35:26 +0330

    M V 1.7

--- jobinja/user/models.py
+++ jobinja/user/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length = 100)
+    phone = models.CharField(max_length = 100)
+    national_id = models.CharField(max_length= 11, unique = True)
+    email = models.EmailField(null = True, blank = True)
+
+class profile(models.Model):
+    user = models.OneToOneField(to=User, on_delete= models.CASCADE)
+    avatar = models.ImageField(upload_to= 'profile_pics/')

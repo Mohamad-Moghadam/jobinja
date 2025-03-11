91af12329d64beedf4b73d6927074d9f4506e7ff
amirhoseinheydari <amirhoseinhydri.1381@gmail.com>
2025-03-11 23:47:34 +0330

    A V1.4

--- jobinja/job/urls.py
+++ jobinja/job/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import display_job_page
+from .views import display_job_page,job_details
 
 urlpatterns = [
     path("", display_job_page),
-    
+    path("job-details/", job_details)
+
   
 ]
--- jobinja/job/views.py
+++ jobinja/job/views.py
@@ -1,14 +1,24 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, FileResponse
+from . import models
+import os
 
 def display_job_page(request):
     return HttpResponse("Hello and welcome this is job page!")
 
 
 def list_jobs(request):
-    pass
+    jobs = models.Job.objects.filter(status="draft")
+    context = {
+        "jobs":jobs
+    }    
+    return HttpResponse("hello this is jobs!")
 
 
 def job_details(request):
-    pass
+    file_jobs = os.path.join("/home/amirykta/Desktop/practice_jobinja/jobinja/job/jobs_list.pdf")
+    return FileResponse(
+        open(file_jobs, "rb"),
+        as_attachment=True
+    )
+    

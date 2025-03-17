65799e496eef8a394f14f28dbedd31f48569c034
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-03-17 21:51:20 +0330

    ✨ new method for adding occupations added

--- jobinja/job/models.py
+++ jobinja/job/models.py
@@ -8,7 +8,7 @@ class Job(models.Model):
     )
     title = models.CharField(max_length=50)
     owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
-    description = models.TextField()
+    description = models.TextField(null= True, blank= True)
     created_date = models.DateTimeField(auto_now_add= True)
     status = models.CharField(max_length=10, choices=STATUS_CHICES, default='draft')
 
--- jobinja/job/views.py
+++ jobinja/job/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, FileResponse
 from . import models
 import os
+import json
+from django.views.decorators.csrf import csrf_exempt
+from job.models import Job
 
 def display_job_page(request):
     return HttpResponse("Hello and welcome this is job page!")
-
 
 def list_jobs(request):
     jobs = models.Job.objects.filter(status="draft")
@@ -14,10 +16,20 @@ def list_jobs(request):
     }    
     return HttpResponse("hello this is jobs!")
 
-
 def job_details(request):
     file_jobs = os.path.join("/home/amirykta/Desktop/practice_jobinja/jobinja/job/jobs_list.pdf")
     return FileResponse(
         open(file_jobs, "rb"),
         as_attachment=True
     )
+
+@csrf_exempt
+def add_occupation(request):
+    if request == "POST":
+        data = json.loads(request.body)
+        Job.objects.create(
+            title = data.get("title"),
+            user_id = data.get("owner_id"),
+            description = data.get("description"),
+            status = data.get("status"),
+        )

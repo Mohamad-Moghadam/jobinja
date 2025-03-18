7544801b525fa6086613f7e4f4819fd796636526
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-03-18 18:44:11 +0330

    ✨ new view added for sign-up user

--- jobinja/user/urls.py
+++ jobinja/user/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import user_welcome, user_cookies
+from .views import user_welcome, user_cookies, sign_up
 
 
 urlpatterns = [
     path('', user_welcome),
     path('cookies', user_cookies),
+    path('sign-up', sign_up)
 ]
--- jobinja/user/views.py
+++ jobinja/user/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http.response import HttpResponse, FileResponse
 import os
+import json
+from user.models import User
+from django.views.decorators.csrf import csrf_exempt
 
 
 def user_welcome(request):
@@ -9,3 +12,19 @@ def user_welcome(request):
 def user_cookies(request):
     file = os.path.join('C:/Users/mhmmd/Desktop/test1/practice12-4/jobinja/user/templates/user/cookies.txt')
     return FileResponse(open(file, 'rb'), as_attachment=True)
+
+@csrf_exempt
+def sign_up(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        User.objects.create(
+            name = data.get("name"),
+            password = data.get("password"),
+            phone_number = data.get("phone_number"),
+            national_id = data.get("national_id"),
+            email = data.get("email"),
+            employer = data.get("employer"),
+            employee = data.get("employee"),
+            interested_in = data.get("interested_in"),
+        )
+        return HttpResponse(f"{data.get("name")} signed up.")

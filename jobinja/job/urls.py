e4c74a605fe24b3a4491daa4663b5144788bb523
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-03-29 00:25:10 +0330

    🐛 urls fixed

--- jobinja/job/urls.py
+++ jobinja/job/urls.py
@@ -4,5 +4,5 @@ from .views import display_job_page,job_details, add_occupation
 urlpatterns = [
     path("", display_job_page),
     path("job-details/", job_details),
-    path('add-occupation', add_occupation)
+    path('add-occupation/<int:user_id>', add_occupation),
 ]

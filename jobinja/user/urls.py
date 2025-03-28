851a6fb470df8475b358ff109658375e30de2e80
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-03-29 00:17:47 +0330

    ✅ sign_up test case passed

--- jobinja/user/urls.py
+++ jobinja/user/urls.py
@@ -5,5 +5,5 @@ from .views import user_welcome, user_cookies, sign_up
 urlpatterns = [
     path('', user_welcome),
     path('cookies', user_cookies),
-    path('sign-up', sign_up)
+    path('sign-up', sign_up),
 ]

7d5baeba7ba083067562f263609edab8aed31f74
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-03-29 00:19:32 +0330

    🐛 job model fixed

--- jobinja/job/models.py
+++ jobinja/job/models.py
@@ -7,7 +7,7 @@ class Job(models.Model):
         ("published", "Published")
     )
     title = models.CharField(max_length=50)
-    owner = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    owner = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name= "the_employer")
     description = models.TextField(null= True, blank= True)
     created_date = models.DateTimeField(auto_now_add= True)
     status = models.CharField(max_length=10, choices=STATUS_CHICES, default='draft')

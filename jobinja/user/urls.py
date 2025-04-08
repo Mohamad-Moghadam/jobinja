460a09db65e23b28e55e4a9a6293979d6450106a
Mobinamousavii <mobinamsv6@gmail.com>
2025-04-09 00:06:03 +0330

    login urls added

--- jobinja/user/urls.py
+++ jobinja/user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import user_welcome, user_cookies, sign_up
+from .views import user_welcome, user_cookies, sign_up , login
 
 
 urlpatterns = [
     path('', user_welcome),
     path('cookies', user_cookies),
     path('sign-up', sign_up),
+    path('login',login),
 ]

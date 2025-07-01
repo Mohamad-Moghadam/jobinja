a27d8f861f469bded87377371f3a2ce417fb8f7b
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-07-01 19:29:23 +0330

    ✨ serializers fixed

--- jobinja/user/serializers.py
+++ jobinja/user/serializers.py
@@ -7,12 +7,3 @@ class UserSerializer(ModelSerializer):
         model = User
         fields = "__all__"
         write_only_fields= ['password']
-    
-    def validate(self, attrs):
-        validated_data = super().validate(attrs)
-        validated_data["password"] = make_password(validated_data["password"])
-        return validated_data
-        
-
-
-

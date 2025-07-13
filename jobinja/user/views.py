1f9febfc6f6fbe1d8d47317d1e674d50234dedcf
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-07-13 16:30:51 +0330

    ✨ jwt timer added

--- jobinja/user/views.py
+++ jobinja/user/views.py
@@ -2,6 +2,7 @@ from rest_framework.generics import CreateAPIView
 from .permissions import IsSuperuser
 from .serializers import UserSerializer
 from django.contrib.auth.models import User
+from django.contrib.auth.models import Group
 
 
 class NewSuperuser(CreateAPIView):
@@ -12,4 +13,6 @@ class NewSuperuser(CreateAPIView):
         return User.objects.filter(groups__name = "Superuser")
 
     def perform_create(self, serializer):
-        User.is_superuser = True
+        user = serializer.save(is_superuser=True, is_staff=True, is_active=True)
+        group, _ = Group.objects.get_or_create(name="Superuser")
+        user.groups.add(group)

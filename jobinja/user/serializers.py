2e859d4355e9dcd4776df9e9b47b5443c757f886
Mohamad Reza <m.reza.s.moghadam@gmail.com>
2025-08-11 02:00:43 +0330

    🐛 getting access token fixed for created superusers by views

--- jobinja/user/serializers.py
+++ jobinja/user/serializers.py
@@ -6,4 +6,14 @@ class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = "__all__"
-        write_only_fields= ['password']
+        extra_kwargs = {
+            'password' : {'write_only': True}
+        }
+        
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        user = self.Meta.model(**validated_data)
+        if password:
+            user.set_password(password)
+        user.save()
+        return user
